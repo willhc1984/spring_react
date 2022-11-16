43791e38b5a2223cd84f9a5aa5fbef09c7b0339b
will henrique <willhc1984@gmail.com>
2022-11-16 13:42:15 -0300

    Setup inicial projeto Spring

---
+++ backend/api_spring_react/src/test/java/br/com/api/ApiSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
