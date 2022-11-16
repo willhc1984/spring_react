bdd0823fdead53fc528ee610d96f0a96ec676c17
will henrique <willhc1984@gmail.com>
2022-11-16 14:32:53 -0300

    Produtos - Service/Controller - get'

--- backend/api_spring_react/src/main/java/br/com/api/controller/ProdutoController.java
+++ backend/api_spring_react/src/main/java/br/com/api/controller/ProdutoController.java
@@ -1,16 +1,35 @@
 package br.com.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.model.ProdutoModel;
+import br.com.api.service.ProdutoService;
 
 @RestController
 @RequestMapping(value = "/produtos")
 public class ProdutoController {
 	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	@GetMapping
-	public String rota() {
-		return "API rota produtos OK";
+	public ResponseEntity<List<ProdutoModel>> listarTodos() {
+		return ResponseEntity.status(HttpStatus.OK).body(produtoService.listarTodos());
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProdutoModel> listar(@PathVariable Long id){
+		Optional<ProdutoModel> produtoOptional = produtoService.buscar(id);
+		return ResponseEntity.status(HttpStatus.OK).body(produtoOptional.get());
 	}
 
 }
---
+++ backend/api_spring_react/src/main/java/br/com/api/service/ProdutoService.java
@@ -0,0 +1,28 @@
+package br.com.api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.model.ProdutoModel;
+import br.com.api.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	public List<ProdutoModel> listarTodos(){
+		return produtoRepository.findAll();
+	}
+
+	public Optional<ProdutoModel> buscar(Long id) {
+		return produtoRepository.findById(id);
+	}
+	
+
+
+}
