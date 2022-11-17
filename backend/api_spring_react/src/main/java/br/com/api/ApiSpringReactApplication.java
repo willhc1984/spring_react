ce0296af3514fef3e4c951b2837bd4aecafa13ce
will henrique <willhc1984@gmail.com>
2022-11-17 09:35:08 -0300

    Teste de liberação CORS com javascript

--- backend/api_spring_react/src/main/java/br/com/api/ApiSpringReactApplication.java
+++ backend/api_spring_react/src/main/java/br/com/api/ApiSpringReactApplication.java
@@ -1,13 +1,33 @@
 package br.com.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.api.model.ProdutoModel;
+import br.com.api.repository.ProdutoRepository;
+
 @SpringBootApplication
-public class ApiSpringReactApplication {
+public class ApiSpringReactApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringReactApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ProdutoModel p1 = new ProdutoModel(1L, "TV", "LG");
+		ProdutoModel p2 = new ProdutoModel(2L, "Computador", "IBM");
+		ProdutoModel p3 = new ProdutoModel(3L, "Celular", "Samsung");
+		ProdutoModel p4 = new ProdutoModel(4L, "Microondas", "LG");
+		ProdutoModel p5 = new ProdutoModel(5L,"Notebook", "Acer");
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));		
 	}
 
 }
--- backend/api_spring_react/src/main/java/br/com/api/controller/ProdutoController.java
+++ backend/api_spring_react/src/main/java/br/com/api/controller/ProdutoController.java
@@ -1,12 +1,12 @@
 package br.com.api.controller;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +21,7 @@ import br.com.api.service.ProdutoService;
 
 @RestController
 @RequestMapping(value = "/produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 	
 	@Autowired
