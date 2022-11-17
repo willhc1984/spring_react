fefc1bf73d098dd431cae4bdc7d89368bdf219d4
will henrique <willhc1984@gmail.com>
2022-11-17 11:25:23 -0300

    Carregando dados na tabela - fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+
+  // UseState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  // UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/produtos")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
   return (
     <div>
-      <Formulario />
-      <Tabela />
+      <Formulario botao={btnCadastrar} />
+      <Tabela vetor={produtos} />
     </div>
   );
 }
--- frontend/src/Formulario.js
+++ frontend/src/Formulario.js
@@ -1,13 +1,22 @@
-function Formulario(){
+function Formulario({botao}){
     return(
         <form>
             <input type='text' className='form-control' placeholder='Nome' />
             <input type='text' className='form-control' placeholder='Marca' />
 
-            <input type='button' className='btn btn-primary' value='Cadastrar' />
-            <input type='button' className='btn btn-warning' value='Alterar' />
-            <input type='button' className='btn btn-danger' value='Remover' />
-            <input type='button' className='btn btn-secondary' value='Cancelar' />
+            {
+                botao
+                ?
+                <input type='button' className='btn btn-primary' value='Cadastrar' />
+                :
+                <div>
+                    <input type='button' className='btn btn-warning' value='Alterar' />
+                    <input type='button' className='btn btn-danger' value='Remover' />
+                    <input type='button' className='btn btn-secondary' value='Cancelar' />
+                </div>
+            }
+            
+            
         </form>
     )
 }
